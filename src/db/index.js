079049e5b173c704825c8b8e954e69cde087fccc
AlanEzequiel112 <cardiello.ezequiel@gmail.com>
2026-03-19 02:59:27 -0300

    chore: configurar base de datos en Railway y script de inicialización

--- src/db/index.js
+++ src/db/index.js
@@ -2,6 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  client_encoding: 'UTF8',
 });
 
 pool.on('connect', () => {
